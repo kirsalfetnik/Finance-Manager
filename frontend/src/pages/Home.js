99602ec73bd82974bdde6154c0392ed0faeded67
Kirill Salfetnik <oriole001@gmail.com>
2022-09-08 23:05:34 +0200

    Start working on the frontend

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,31 @@
+import { useEffect, useState } from 'react';
+
 const Home = () => {
+    const [months, setMonths] = useState(null)
+
+    useEffect(() => {
+        const fetchMonths = async () => {
+            const response = await fetch('/api/months')
+            const json = await response.json()
+
+            if (response.ok) {
+                setMonths(json)
+            }
+        }
+
+        fetchMonths()
+    }, [])
+
     return (
         <div className="home">
-            <h2>Home</h2>
+            <div className="months">
+                {months && months.map((month) => (
+                    <p key={month._id}>{month.name}</p>
+                ))}
+            </div>
         </div>
     )
 }
+
 
 export default Home;
